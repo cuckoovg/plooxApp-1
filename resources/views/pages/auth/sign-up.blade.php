6a7a549d6f8cef9d691abd0eb71e2a94198ea521
cuckoovg <galihtrisnantoro@gmail.com>
2025-04-24 12:05:28 +0800

    update 0.1

--- resources/views/pages/auth/sign-up.blade.php
+++ resources/views/pages/auth/sign-up.blade.php
@@ -12,7 +12,7 @@ use App\Classes\Hook;
             <div id="sign-in-box" class="w-full md:w-3/5 lg:w-2/5">
                 <div class="flex justify-center items-center py-6">
                     @if ( ! ns()->option->get( 'ns_store_square_logo', false ) )
-                    <img class="w-32" src="{{ asset( 'svg/nexopos-variant-1.svg' ) }}" alt="NexoPOS">
+                    <img class="w-64" src="{{ asset( 'svg/plooxPOS.svg' ) }}" alt="NexoPOS">
                     @else
                     <img src="{{ ns()->option->get( 'ns_store_square_logo' ) }}" alt="NexoPOS">
                     @endif
